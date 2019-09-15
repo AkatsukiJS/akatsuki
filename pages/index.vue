f436ac91969e1014f82a169229b419c3ddbad915
jeovazero <jeovapereira2000@live.com>
2019-09-14 23:06:35 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,25 @@
           Sobre Nós
         </h1>
         <p>
-          Somos uma organização formada por alguns entusiastas JS e zueiros. Nós
-          fazemos alguns projetos apenas para aprendizado e diversão. Nós só
-          usamos JavaScript <span class="about-text__heart">♥</span>.
+          Somos um grupo formado por entusiastas de JavaScript, zueiros e
+          adoradores de memes <span class="about-text__heart">♥</span>. Temos o
+          objetivo de aprender coisas do multiverso JS e arredores, podendo às
+          vezes também sair da zona de conforto e disseminar conhecimento de
+          alguma forma. Acredite, isto pode acontecer.
         </p>
+        <p>
+          Para isso, faremos mini projetos ou posts no blog (sabemos fazer um
+          blog com Nuxt). Esta é uma forma de engajar os participantes do grupo
+          a fazer algum projeto aleatório (sem compromisso com nada) por meio da
+          tripla aprender-fazer-disseminar. Isto pode funcionar às vezes, mas
+          geralmente não, é só acreditar.
+        </p>
+        <p><i>&rsaquo;</i> Considerações</p>
+        <ul>
+          <li v-for="item in items" :key="item[0]">
+            <i>{{ item[0] }} .</i> {{ item[1] }}
+          </li>
+        </ul>
         <p>
           Em breve, projetos interessantes!
         </p>
@@ -31,6 +46,26 @@ export default {
     HeaderDark,
     FooterX,
     LogoAnimated
+  },
+  computed: {
+    items() {
+      return [
+        [
+          1,
+          `Não somos restritos apenas a JS (os arredores inclui os
+          seus flavors/transpilers)`
+        ],
+        [2, 'Não temos medo de errar'],
+        [3, 'Levamos tudo na zueira, nosso lema: "Fazemos tudo só de zoas"'],
+        [
+          4,
+          `Não espere nada do grupo, fazemos algo quando você menos espera
+           ou quando você achar que o grupo morreu (gostamos de surpreender)`
+        ],
+        [5, 'JS > Python, se afoba não, vai para 3'],
+        [0, 'Achamos que usar JavaScript é terrível e frustante']
+      ]
+    }
   }
 }
 </script>
@@ -59,12 +94,16 @@ export default {
     border-radius: 8px
     @media #{$breakpoint-2}
       @include font-size-large
-  & p
+  & p, & ul
     @include typho-strong-1
     @include font-size-large
     padding: 1rem 0
     @media #{$breakpoint-2}
       @include font-size-medium
+  & li
+    padding: 0 0 0.5rem
+  & i
+    color: pallete("secondary")
   &__heart
     color: pallete("secondary")
     animation: heart-color 1s ease infinite
